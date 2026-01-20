1dbd92e32e8e20f1df2bf42e32bfce38878e40db
Ndzalo-Mathumbu <ndzalonkmathumbu@gmail.com>
2026-01-20 18:17:00 +0200

    completed challeng 1 building a react card

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 // import PizzaFocaccia from "03-pizza-menu/public/pizzas/focaccia.jpg";
 
 const pizzaData = [
@@ -161,8 +161,100 @@ const Footer = function () {
 };
 
 ///// Render the root or the app in the DOM /////
-ReactDom.createRoot(document.getElementById("root")).render(
+/* ReactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+); */
+
+///// Challenge No.1 building a developer card /////
+
+const App2 = function () {
+  return (
+    <>
+      <Card />
+    </>
+  );
+};
+
+const Card = function () {
+  const styleCard = {
+    width: "390px",
+    height: "460px",
+    backgroundColor: "blue",
+    margin: "0 auto",
+    borderRadius: "5px",
+    border: "2px solid black",
+  };
+  const styleIMG = {
+    width: "100%",
+    height: "55%",
+    objectFit: "cover",
+  };
+
+  const styleName = {
+    textAlign: "center",
+    color: "white",
+    marginTop: "7px",
+    fontFamily: "Inter",
+  };
+
+  const styleInfo = {
+    textAlign: "center",
+    color: "white",
+    marginTop: "-7px",
+    fontFamily: "Inter",
+    padding: "1px",
+  };
+
+  return (
+    <div style={styleCard}>
+      <img
+        style={styleIMG}
+        src="https://velaranks.com/wp-content/uploads/2024/12/Software-Development.webp"
+        alt="coder"
+      />
+      <h2 style={styleName}>John Doe</h2>
+      <h3 style={styleInfo}>
+        John Doe is a coder who writes bugs professionally and occasionally
+        fixes them by accident. Debugging is his cardio, coffee is his fuel, and
+        “it works on my machine” is his battle cry.
+      </h3>
+      <div
+        style={{
+          display: "flex",
+          gap: "2px",
+          position: "relative",
+          left: "-4.9px",
+        }}
+      >
+        <Skills skillName="HTML + CSS🎨" color="red" />
+        <Skills skillName="Javascript🤖" color="orange" />
+        <Skills skillName="React😀" color="skyblue" />
+      </div>
+    </div>
+  );
+};
+
+const Skills = function (props) {
+  const styleSkills = {
+    backgroundColor: props.color,
+    width: "150px",
+    borderRadius: "5px",
+    textAlign: "center",
+    fontFamily: "Inter",
+    color: "white",
+    position: "relative",
+    bottom: "13px",
+    left: "5px",
+    // display: "flex",
+    // marginTop: "3px",
+  };
+  return <div style={styleSkills}>{props.skillName}</div>;
+};
+
+ReactDom.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App2 />
   </React.StrictMode>
 );
