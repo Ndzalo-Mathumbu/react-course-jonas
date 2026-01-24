15b9f6976a0cea2e10bd8cba10e85770b5d7fa9a
Ndzalo-Mathumbu <ndzalonkmathumbu@gmail.com>
2026-01-24 21:38:45 +0200

    completed a useState exercise

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,10 +1,6 @@
 import { useState } from "react";
 
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-];
+const messages = ["Wake Up 🛏", "Bath 🛁", "Go To School 🏫"];
 
 const App = function () {
   const [stepIN, setStep] = useState(1);
@@ -17,7 +13,7 @@ const App = function () {
   };
 
   const handleCard = function () {
-    setCard(!card);
+    setCard((cardOpen) => !cardOpen);
   };
 
   // const stepIN = 1;
@@ -63,4 +59,87 @@ const App = function () {
   );
 };
 
-export default App;
+// export default App;
+
+const App2 = function () {
+  return (
+    <>
+      <DateCounter />
+    </>
+  );
+};
+
+const DateCounter = function () {
+  const [countnum, setCountnum] = useState(0);
+  const [stepnum, setStepnum] = useState(1);
+  const [date, setDate] = useState(new Date());
+  const stepdiv = {
+    display: "flex",
+    margin: "0 auto",
+    width: "10vw",
+    // backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "5px",
+    height: "8vh",
+  };
+  const styleText = {
+    display: "flex",
+    margin: "0 auto",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  const handleAddCounter = () => {
+    setCountnum((a) => a + stepnum);
+  };
+  const handleMinusCounter = () => {
+    setCountnum((a) => a - stepnum);
+  };
+
+  const handleAddStep = () => {
+    setStepnum((a) => a + 1);
+  };
+  const handleMinusStep = () => {
+    setStepnum((a) => a - 1);
+  };
+
+  /* const datecount = new Date();
+
+  const dateFomat = new Intl.DateTimeFormat("en-ZA", {
+    weekday: "short",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).formatToParts(datecount);
+
+  const day = dateFomat.find((x) => x.type === "day").value;
+
+  console.log(+day + 1);
+
+  console.log(dateFomat); */
+
+  /* const handleAddDate = function () {
+    const myDate = new Date(date);
+    myDate.setDate(() => myDate.getDate() + 1);
+    setDate(myDate);
+    console.log(myDate);
+  }; */
+  return (
+    <>
+      <div style={stepdiv}>
+        step: {stepnum} <button onClick={handleAddStep}>+</button>
+        <button onClick={handleMinusStep}>-</button>
+      </div>
+      <div style={stepdiv}>
+        Count: {countnum}
+        <button onClick={handleAddCounter}>+</button>
+        <button onClick={handleMinusCounter}>-</button>
+      </div>
+      <p style={styleText}>
+        {countnum} Days From Today Is Friday {/* {date.getDate()} */}
+      </p>
+    </>
+  );
+};
+
+export default App2;
