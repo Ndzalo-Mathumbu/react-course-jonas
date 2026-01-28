476d9be4d4871121c60e2c5f3135982efc643c17
Ndzalo-Mathumbu <ndzalonkmathumbu@gmail.com>
2026-01-28 23:24:59 +0200

    Implement packing list app with state lifting and controlled inputs

--- 05-travel-list/src/components/App.jsx
+++ 05-travel-list/src/components/App.jsx
@@ -3,6 +3,7 @@ import Logo from "./Logo";
 import Form from "./Form";
 import PackageList from "./PackingList";
 import Stats from "./Stats";
+import "../style.css";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -195,3 +196,61 @@ const App2 = function () {
 
 export default App2;
  */
+
+///// Practice Exercise 👇 /////
+
+const faqs = [
+  {
+    // id: Date.now(),
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    // id: Date.now(),
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    // id: Date.now(),
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+const AppTwo = function () {
+  return (
+    <div>
+      <Accordion data={faqs} />
+      {/* <AccordionItem /> */}
+    </div>
+  );
+};
+
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((a, i) => (
+        <AccordionItem title={a.title} answer={a.text} num={i + 1} key={i} />
+      ))}
+    </div>
+  );
+}
+
+const AccordionItem = function ({ num, title, answer }) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = function () {
+    setOpen((a) => !a);
+  };
+  return (
+    <>
+      <div className={`item ${open ? "open" : ""}`} onClick={handleOpen}>
+        <p className="number">{num < 9 ? `0${num}` : num}</p>
+        <p className="title">{title}</p>
+        <p className="icon">{open ? `+` : `-`}</p>
+      </div>
+      {open && <div className="content-box">{answer}</div>}
+    </>
+  );
+};
+
+// export default AppTwo;
